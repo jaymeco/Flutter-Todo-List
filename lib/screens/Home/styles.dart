36a0dba230dbc2dace6a2bae811117849ba30c2c
jaymeco <jayme.anunciacao@gmail.com>
2022-09-07 18:22:33 -0300

    feature(lib/screens/Home): Adiciona listagem local de tarefas.
    - Implementa adição de tarefas de forma local.

--- lib/screens/Home/styles.dart
+++ lib/screens/Home/styles.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 class Styles {
   BoxDecoration homeContainer() {
-    return const BoxDecoration(
-      color: Colors.white12,
+    return BoxDecoration(
+      color: Colors.grey.shade100,
     );
   }
 
   TextStyle pageTitle() {
     return const TextStyle(
-      color: Colors.black,
+      color: Colors.white,
       fontWeight: FontWeight.bold,
       fontSize: 30,
     );
@@ -17,8 +17,17 @@ class Styles {
 
   TextStyle pageSubtitle() {
     return const TextStyle(
-      color: Colors.black54,
+      color: Colors.white60,
       fontSize: 15,
+    );
+  }
+
+  InputDecoration addTaskField() {
+    return const InputDecoration(
+      hintText: 'Adicionar tarefa',
+      contentPadding: EdgeInsets.symmetric(horizontal: 8),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50))),
     );
   }
 }
