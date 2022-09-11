be6abf309c9dca68301d5ac909cc7c561c673ff3
jaymeco <jayme.anunciacao@gmail.com>
2022-09-10 22:51:21 -0300

    feature(lib/main): Adiciona estilização da statusbar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './screens/Home/index.dart';
 
 void main() {
@@ -11,7 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.blue.shade400,
+      )
+    );
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
